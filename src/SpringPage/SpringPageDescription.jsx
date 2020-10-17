1e1237633688a01086b3117e06d62b2e56c5cb50
Stygmates <styg.tan.dat@gmail.com>
2020-10-18 01:50:46 +0200

    Updated SpringPageDescription

--- src/SpringPage/SpringPageDescription.jsx
+++ src/SpringPage/SpringPageDescription.jsx
@@ -5,7 +5,8 @@ const SpringPageDescription = () => {
         <h2 style={{textAlign: "center"}}>About this page</h2>
         <div className='container' style={{ width: '60%'}}><hr/></div>
         <div style={{display: "inline-block", width: "80%", paddingLeft: "50px"}}>
-        <h5>This page presents my experiments with spring boot: The React front end interacts with the Spring boot back end, as of now, we can interact with it by adding users to an HashMap.</h5>
+        <h5>This page presents my experiments with spring boot: The React front end interacts with the Spring boot back end, as of now, we can interact with it by adding users to an HashMap with the Username as key.
+            It is stored on the Spring boot server side. The list of saved users is then fetched from the Spring boot server once the form is sent.</h5>
         </div>
     </div>
 }
