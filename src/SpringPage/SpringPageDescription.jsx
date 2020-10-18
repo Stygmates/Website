11751650bf2868ee908a6d1c2741a65cc49aaaca
Stygmates <styg.tan.dat@gmail.com>
2020-10-18 02:44:00 +0200

    Fixed missplacement

--- src/SpringPage/SpringPageDescription.jsx
+++ src/SpringPage/SpringPageDescription.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 const SpringPageDescription = () => {
 
-    return <div id="SpringPageDescription">
-        <h2 style={{textAling: "center", paddingTop: "25px", paddingBottom: "25px"}}>About this page</h2>
+    return <div id="SpringPageDescription" style={{paddingTop: "25px", paddingBottom: "25px"}}>
+        <h2>About this page</h2>
         <div className='container' style={{ width: '60%'}}><hr/></div>
         <div style={{display: "inline-block", width: "80%", paddingLeft: "50px"}}>
         <h5>This page presents my experiments with spring boot: The React front end interacts with the Spring boot back end, as of now, we can interact with it by adding users to an HashMap with the Username as key.
