6a1ea8164d1c421996b26689ce45fc3f5e679b59
Stygmates <styg.tan.dat@gmail.com>
2025-03-26 11:23:14 +0100

    Update AboutMe.jsx

--- src/HomePage/AboutMe.jsx
+++ src/HomePage/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
         <div className='container' style={{ width: '60%'}}><hr/></div>
         <div style={{display: "inline-block", width: "80%", paddingLeft: "50px"}}>
         <h3>Tan Dat Truong</h3>
-        <h5>Project manager</h5>
+        <h5>Software engineer</h5>
         <h6>I graduated with a Master's Degree in Computer Sciences in 2019 with a background in Image Processing, I've been working as a frontend/backend engineer for the past 5 years. I have been using Linux and Windows as my operating systems and I'm also familiar with several concepts and tools such as Test Driven Development(PyTest/GoogleTest), Continuous Integration(TravisCI/GitlabCI), and Git.</h6>
         <h6>I created this site using:</h6>
         <div style={{ textAlign: "center"}}>
